96f02507054a039cf9ef301d1620195fb2cef754
TalithaSevrillaD <42019785+TalithaSevrillaD@users.noreply.github.com>
2019-03-18 10:17:15 +0700

    Add files via upload

--- read.php
+++ read.php
@@ -1,14 +1,16 @@
 <?php
+
+	session_start();
+	
+	if(!(isset($_SESSION['user'])))
+	{
+		header("location: ../login/form-login.php");
+	}
 	
 	include '../connect.php';
 	
-	$query = "SELECT kode, nama_matkul, sks, semester, nama_dosen
-			FROM matakuliah LEFT JOIN dosen
-			USING (id_dosen)
-			ORDER BY kode";
-			
+	$query = "SELECT * FROM dosen";
 	$result = mysqli_query($connect, $query);
-	
 	$num = mysqli_num_rows($result);
 	
 ?> 
@@ -18,45 +20,38 @@
 		<title></title>
 	</head>
 	<body>
-		<table border = "1">
+		<table border = '1'>
+		<h2>Data dosen</h2>
 		<tr>
 			<th>No.</th>
-			<th>Kode</th>
-			<th>Matakuliah</th>
-			<th>SKS</th>
-			<th>Semester</th>
-			<th>Dosen Pengajar</th>
+			<th>Nama Dosen</th>
+			<th>Telepon</th>
 			<th>Aksi</th>
 		</tr>
 		<?php	
 			if($num > 0){
 				$no = 1;
-				while($data = mysqli_fetch_assoc($result)){ ?>
-				
-					<tr>
-						<td><?php echo $no; ?></td>
-						<td><?php echo $data['kode']?></td>
-						<td><?php echo $data['nama_matkul']?></td>
-						<td><?php echo $data['sks']?></td>
-						<td><?php echo $data['semester']?></td>
-						<td><?php
-								if($data['nama_dosen'] != NULL)
-								{ echo $data['nama_dosen'];}
-								else {echo "NULL";}
-							?>
-						<td>
-							<a href="form-update.php?kode=<?php echo $data['kode']; ?>">Edit</a> | 
-							<a href="delete.php?kode=<?php echo $data['kode']; ?>" onclick="return confirm('Anda yakin ingin menghapus data?')">Hapus</a>
-						</td>
-					</tr>
-					<?php
+				while($data = mysqli_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$no."</td>";
+					echo "<td>".$data['nama_dosen']. "</td>";
+					echo "<td>".$data['telp']. "</td>";
+					echo "<td><a href='form-update.php?id_dosen= " .$data['id_dosen']. "'>Edit</a> | ";
+					echo "<td><a href = 'delete.php?id_dosen=" .$data['id_dosen']."' onclick='return confirm(\"Apakah Anda yakin ingin menghapus data?\")'>Hapus</a></td>";
 					$no++;
 				}
 			}
 			else {
-				echo "<td colspan = '7'>Tidak ada data</td>";
+				echo "<td colspan = '3'>Tidak ada data</td>";
 			}
 		?>
 		</table>
+		<form action = "../login/logout.php">
+		<table>
+			<tr>
+				<td><input type="submit" value="Logout"></td>
+			</tr>
+		</table>
+		</form>
 	</body>
 </html>
